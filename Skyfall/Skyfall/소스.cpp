37db82f79cfb843bfdc2c0bd5851f5c680f5e2b0
Kwakdohun <gamenot99@naver.com>
2015-03-29 16:48:39 +0900

    0329

--- Skyfall/Skyfall/소스.cpp
+++ Skyfall/Skyfall/소스.cpp
@@ -1,8 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+char card[4][13] = { { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' }, { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' }, { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' }, { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' } };
+void suits(int i) // 카드 모양 함수
+{
+	switch (i)
+	{
+	case 0:  printf("♠,"); break;
+	case 1:  printf("♥,"); break;
+	case 2:  printf("◆,"); break;
+	case 3:  printf("♣,"); break;
+	}
+
+}
 void main()
 {
-	char a[20] = "Hello WoRid";
-
-	printf("%s\n", a); //확인 중
-	printf("%c\n", a[3]);
+	int i, j;
+	int num1, num2; // num1 =모양 , num2 = 숫자 
+	srand(time(NULL)); // 시간마다 랜덤함수를 뽑음
+	for (i = 0; i < 4; i++){
+		suits(i);
+		for (j = 0; j < 13; j++)
+			printf("%c ", card[i][j]);
+		printf("\n");
+	}
+	num1 = rand() % 4; // 0~ 3까지 숫자를 뽑음
+	num2 = rand() % 13; // 0 ~  13 까지 숫자를 뽑음
+	printf("\n\n");
+	suits(num1);
+	printf("%c", card[num1][num2]);
 }
