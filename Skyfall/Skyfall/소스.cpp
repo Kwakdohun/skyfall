d901986316a8ec2d6a6039cdf4fedbf12617aeec
Kwakdohun <gamenot99@naver.com>
2015-03-30 19:19:17 +0900

    변경

--- Skyfall/Skyfall/소스.cpp
+++ Skyfall/Skyfall/소스.cpp
@@ -1,44 +1,95 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-char card[4][13] = { { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '@', 'J', 'Q', 'K' }, 
-					 { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '@', 'J', 'Q', 'K' }, 
-					 { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '@', 'J', 'Q', 'K' }, 
-					 { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '@', 'J', 'Q', 'K' } }; //카드 설정
-void suits(int i) // 카드 모양 함수
-{
-	switch (i)
-	{
-	case 0:  printf("♠,"); break;
-	case 1:  printf("♥,"); break;
-	case 2:  printf("◆,"); break;
-	case 3:  printf("♣,"); break;
-	}
 
-}
-void cards() //카드 패
-{
-	int i, j;
-	for (i = 0; i < 4; i++){
-		suits(i);
-		for (j = 0; j < 13; j++)
-			printf("%c ", card[i][j]);
-		printf("\n");
-	}
-}
 void main()
 {
-	int i, j;
-	int num1, num2; // num1 =모양 , num2 = 숫자 
-	srand(time(NULL)); // 시간마다 랜덤함수를 뽑음
-	for (int z = 0; z < 3; z++)
+	char player[50];
+	char *pmark[5];//게임중 나타나는 
+	int dealer;
+	char Sdeck[13] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K' }; //일단 A는 1이라고만 생각함
+	char Cdeck[13] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K' };
+	char Ddeck[13] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K' };
+	char Hdeck[13] = { 'A', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'J', 'Q', 'K' };
+
+	char mark[4][4] = { "♤", "♧", "◇", "♡" };
+
+	int i, j;//카드를 선택하기위한 변수
+	int k, l;
+	int sum = 0;
+	char cho;
+
+	srand(time(NULL));
+
+
+	for (k = 0; k < 2; k++)
 	{
-		num1 = rand() % 4; // 0~ 3까지 숫자를 뽑음
-		num2 = rand() % 13; // 0 ~  13 까지 숫자를 뽑음
-		printf("\n\n");
-		suits(num1); // 카드모양 뽑기 
-		printf("%c\n", card[num1][num2]); // 카드숫자 뽑기
-		card[num1][num2] = 0; // 이미 뽑았으니 없애기
-		cards();
+		i = rand() % 4;  //여기서 앞의 문양이 결정
+		j = rand() % 13;
+
+		if (i == 0) //문양에 따라서 숫자값을 가져감
+			player[k] = Sdeck[j];
+		else if (i == 1)
+			player[k] = Cdeck[j];
+		else if (i == 2)
+			player[k] = Ddeck[j];
+		else
+			player[k] = Hdeck[j];
+		sum = sum + (j + 1);
+
+		printf("%s%c ", &mark[i], player[k]);
+	}
+
+	while (1) //플레이어의 패
+	{
+
+
+		if (sum < 15)
+		{
+			i = rand() % 4;  //여기서 앞의 문양이 결정
+			j = rand() % 13;
+
+			if (i == 0) //문양에 따라서 숫자값을 가져감
+				player[k] = Sdeck[j];
+			else if (i == 1)
+				player[k] = Cdeck[j];
+			else if (i == 2)
+				player[k] = Ddeck[j];
+			else
+				player[k] = Hdeck[j];
+
+			sum = sum + (j + 1);
+			printf("%s%c ", &mark[i], player[k]);
+
+		}
+		else
+		{
+			printf("카드를 더 받을지 선택하시오(Y/N) :  ");
+			fflush(stdin);
+			scanf("%c", &cho);
+			if (cho == 'y' || cho=='Y') //한번더 받고싶으면 더받음
+			{
+
+				i = rand() % 4;  //여기서 앞의 문양이 결정
+				j = rand() % 13;
+
+				if (i == 0) //문양에 따라서 숫자값을 가져감
+					player[k] = Sdeck[j];
+				else if (i == 1)
+					player[k] = Cdeck[j];
+				else if (i == 2)
+					player[k] = Ddeck[j];
+				else
+					player[k] = Hdeck[j];
+
+				sum = sum + (j + 1);
+				printf("%s%c ", &mark[i], player[k]);
+
+			}
+			else if (cho == 'N' || cho == 'n')
+				break; // 받기 싫으면 와일문에서 빠져나감
+		}
+
+
 	}
 }
